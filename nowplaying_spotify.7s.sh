8baead10ceffba3458461dda59fe7abdbcbeeb58
Florian Loch (D059349) <florian.loch@sap.com>
2018-01-17 23:37:12 +0100

    Fix indentation

--- nowplaying_spotify.7s.sh
+++ nowplaying_spotify.7s.sh
@@ -39,58 +39,58 @@ fi
 
 # play/pause
 if [ "$1" = "play" ] || [ "$1" = "pause" ]; then
-	osascript -e "tell application \"Spotify\" to $1"
-	exit
+  osascript -e "tell application \"Spotify\" to $1"
+  exit
 fi
 # next/previous
 if [ "$1" = "next" ] || [ "$1" = "previous" ]; then
-	osascript -e "tell application \"$2\" to $1 track"
-	# tell spotify to hit "Previous" twice so it actually plays the previous track
-	# instead of just starting from the beginning of the current one
-	if [ "$playing" = "true" ] && [ "$1" = "previous" ]; then
-		osascript -e "tell application \"$2\" to $1 track"
-	fi
-	exit
+  osascript -e "tell application \"$2\" to $1 track"
+  # tell spotify to hit "Previous" twice so it actually plays the previous track
+  # instead of just starting from the beginning of the current one
+  if [ "$playing" = "true" ] && [ "$1" = "previous" ]; then
+    osascript -e "tell application \"$2\" to $1 track"
+  fi
+  exit
 fi
 
 
 # start outputting information to bitbar
 if [ "$playing" = "" ] && [ "$paused" = "" ]; then
-	# nothing is even paused
-	echo "🙉 No music playing | color=gray"
+  # nothing is even paused
+  echo "🙉 No music playing | color=gray"
 else
-	# something is playing or is paused
-	track=""
-	artist=""
+  # something is playing or is paused
+  track=""
+  artist=""
   album=""
 
-	if [ "$playing" = "" ]; then
-		echo "Spotify is paused | color=#888888"
-		echo "---"
-	fi
+  if [ "$playing" = "" ]; then
+    echo "Spotify is paused | color=#888888"
+    echo "---"
+  fi
 
-	track_query="name of current track"
-	artist_query="artist of current track"
+  track_query="name of current track"
+  artist_query="artist of current track"
   album_query="album of current track"
 
-	# output the track and artist
-	track=$(osascript -e "tell application \"Spotify\" to $track_query")
-	artist=$(osascript -e "tell application \"Spotify\" to $artist_query")
+  # output the track and artist
+  track=$(osascript -e "tell application \"Spotify\" to $track_query")
+  artist=$(osascript -e "tell application \"Spotify\" to $artist_query")
   album=$(osascript -e "tell application \"Spotify\" to $album_query")
 
-	echo "♪ $(echo $track | awk -F '\ -' '{print $1}') - $artist | length=50 color=#25d34e"
-	echo "---"
+  echo "♪ $(echo $track | awk -F '\ -' '{print $1}') - $artist | length=50 color=#25d34e"
+  echo "---"
   echo "$track"
   echo "On: $album"
-	echo "By: $artist"
+  echo "By: $artist"
   echo "---"
 
-	if [ "$playing" != "" ]; then
-		echo "⏸ Pause | bash='$0' param1=pause refresh=true terminal=false"
-	else
-		echo "▶️ Play | bash='$0' param1=play refresh=true terminal=false"
-	fi
+  if [ "$playing" != "" ]; then
+    echo "⏸ Pause | bash='$0' param1=pause refresh=true terminal=false"
+  else
+    echo "▶️ Play | bash='$0' param1=play refresh=true terminal=false"
+  fi
 
-	echo "⏭ Next | bash='$0' param1=next refresh=true terminal=false"
-	echo "⏮ Previous | bash='$0' param1=previous refresh=true terminal=false"
+  echo "⏭ Next | bash='$0' param1=next refresh=true terminal=false"
+  echo "⏮ Previous | bash='$0' param1=previous refresh=true terminal=false"
 fi
